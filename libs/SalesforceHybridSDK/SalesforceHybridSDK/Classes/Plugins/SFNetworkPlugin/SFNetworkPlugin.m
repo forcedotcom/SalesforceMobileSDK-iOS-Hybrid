5e818d9c47dc1c0893d637648c51e70a6aeba8be
Bharath Hariharan <bhariharan@salesforce.com>
2019-08-05 16:57:40 -0700

    Fixing warnings

--- libs/SalesforceHybridSDK/SalesforceHybridSDK/Classes/Plugins/SFNetworkPlugin/SFNetworkPlugin.m
+++ libs/SalesforceHybridSDK/SalesforceHybridSDK/Classes/Plugins/SFNetworkPlugin/SFNetworkPlugin.m
@@ -109,7 +109,7 @@ static NSString * const kDoesNotRequireAuthentication = @"doesNotRequireAuthenti
             NSString* fileUrl = [fileParam nonNullObjectForKey:kFileUrl];
             NSString* fileName = [fileParam nonNullObjectForKey:kFileName];
             NSData* fileData = [NSData dataWithContentsOfURL:[NSURL URLWithString:fileUrl]];
-            [request addPostFileData:fileData paramName:fileParamName description:nil fileName:fileName mimeType:fileMimeType];
+            [request addPostFileData:fileData paramName:fileParamName fileName:fileName mimeType:fileMimeType params:nil];
         }
     }
     
