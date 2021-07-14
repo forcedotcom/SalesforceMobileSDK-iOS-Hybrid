fa41ce5e6763376da0a1495f4f7ffa7aea5bac26
Wolfgang Mathurin <wmathurin@salesforce.com>
2021-07-14 10:28:09 -0600

    Updating version numbers to 9.2.0

--- libs/SalesforceHybridSDK/SalesforceHybridSDKTestApp/cordova_plugins.js
+++ libs/SalesforceHybridSDK/SalesforceHybridSDKTestApp/cordova_plugins.js
@@ -69,7 +69,7 @@ module.exports = [
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "com.salesforce": "9.1.1",
+    "com.salesforce": "9.2.0",
     "org.apache.cordova.console": "0.2.11"
 }
 // BOTTOM OF METADATA
